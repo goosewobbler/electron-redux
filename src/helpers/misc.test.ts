c22dd44638b6167073bc4a2be35a4bc5d4340f12
McKayla Washburn <mckayla@hey.com>
2020-12-19 18:27:10 -0700

    linting

--- src/helpers/misc.test.ts
+++ src/helpers/misc.test.ts
@@ -3,7 +3,9 @@ import { preventDoubleInitialization } from "./misc";
 test("calling preventDoubleInitialization twice throws", () => {
 	jest.spyOn(console, "error").mockImplementation(jest.fn());
 
-	expect(preventDoubleInitialization).not.toThrowError();
-	expect(preventDoubleInitialization).toThrowError();
-	expect(console.error).toBeCalled();
+	expect(preventDoubleInitialization).not.toThrow();
+	expect(preventDoubleInitialization).toThrow();
+
+	// eslint-disable-next-line no-console
+	expect(console.error).toHaveBeenCalled();
 });
