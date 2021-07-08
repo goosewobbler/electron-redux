150c1103197b88e721aa82035d2095b096595cc3
goosewobbler <goosewobbler@pm.me>
2021-07-08 17:43:08 +0100

    nicer prettier config ;)

--- prettier.config.js
+++ prettier.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-	useTabs: true,
-	tabWidth: 4, // Necessary for prettier to align things, even though we use tabs
-	trailingComma: "all",
-	endOfLine: "lf",
+  printWidth: 120,
+  singleQuote: true,
+  quoteProps: 'consistent',
+  trailingComma: 'all',
 };
