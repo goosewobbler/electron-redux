d70cff5906a24e30e0554d1ed1c62e9a96980119
goosewobbler <goosewobbler@pm.me>
2021-07-08 17:53:18 +0100

    update namespace, please prettier

--- packages/electron-redux/src/helpers/actions.test.ts
+++ packages/electron-redux/src/helpers/actions.test.ts
@@ -1,11 +1,11 @@
-import { stopForwarding, validateAction } from "./actions";
+import { stopForwarding, validateAction } from './actions';
 
-test("stopForwarding should cause validateAction to return false", () => {
-	const action = {
-		type: "mckayla.electron-redux.DNF_TEST",
-		payload: {},
-	};
+test('stopForwarding should cause validateAction to return false', () => {
+  const action = {
+    type: 'electron-redux.DNF_TEST',
+    payload: {},
+  };
 
-	expect(validateAction(action)).toBe(true);
-	expect(validateAction(stopForwarding(action))).toBe(false);
+  expect(validateAction(action)).toBe(true);
+  expect(validateAction(stopForwarding(action))).toBe(false);
 });
