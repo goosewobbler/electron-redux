c84d26c293a1aa77ce5bd0eb3beabc149ec4ffb9
goosewobbler <goosewobbler@pm.me>
2021-07-08 23:51:53 +0100

    update config

--- tests/e2e/webpack.config.js
+++ tests/e2e/webpack.config.js
@@ -1,19 +1,27 @@
-'use strict';
-
 const path = require('path');
 
 module.exports = {
-  resolve: {
-    alias: {
-      electron: false,
-    },
-  },
-
   entry: {
-    renderer: './src/renderer/renderer.js',
+    renderer: './src/renderer/renderer.tsx',
+    main: './src/main/main.ts',
   },
   output: {
     path: path.join(__dirname, 'target'),
     filename: '[name].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+    alias: {
+      electron: false,
+    },
   },
 };
